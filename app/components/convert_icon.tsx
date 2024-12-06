427259dbfe3c5fde9b59862370be97f81d1410c1
Clark Alesna <clark_alesna@hotmail.com>
2024-12-06 18:02:01 +0800

    Initial Commit

---
+++ app/components/convert_icon.tsx
@@ -0,0 +1,9 @@
+const ConvertIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 text-black">
+            <path fill="none" stroke="currentColor" stroke-width="2" d="M4 12a8 8 0 018-8m0 0l-3 3m3-3l3 3M20 12a8 8 0 01-8 8m0 0l3-3m-3 3l-3-3" />
+        </svg>
+    );
+}
+
+export { ConvertIcon };
