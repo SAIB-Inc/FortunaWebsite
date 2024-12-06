06336b36025eed6b67105ae1d2369dd48b1bfff7
ricomiles <ricoquiblat@gmail.com>
2024-12-07 02:20:45 +0800

    feat: Add SelectWalletModal component and integrate wallet selection functionality

---
+++ app/components/select_wallet_modal.tsx
@@ -0,0 +1,29 @@
+import { CardanoWallet } from "@saibdev/bifrost"
+
+interface SelectWalletModalProps {
+    wallets: CardanoWallet[];
+    handleSelectWallet: (wallet: CardanoWallet) => void;
+}
+
+const capitalize = (str: string) => {
+    if (str.length === 0) return str; // handle empty string case
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+const SelectWalletModal = ({ wallets, handleSelectWallet }: SelectWalletModalProps) => {
+    return (
+        <div className="flex gap-4 flex-col w-[300px] h-[400px] bg-[#15191e] drop-shadow-xl rounded-lg p-5">
+            {wallets.map((wallet: CardanoWallet) => (
+                <div className="flex items-center gap-2 justify-center w-full p-2 bg-[#5A66F6] rounded-md cursor-pointer 
+                  hover:bg-[#4E5BE5] active:bg-[#3F4CCB] transition select-none" onClick={() => handleSelectWallet(wallet)}>
+                    <div className="w-[30px]">
+                        <img src={wallet.icon} alt="Fortuna Logo" className="w-full h-full" />
+                    </div>
+                    <span className="text-black font-medium">{capitalize(wallet.name)}</span>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export {SelectWalletModal};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { ConvertIcon } from "~/components/convert_icon";
 import { FishIcon } from "~/components/fish_icon";
-import { getWallets } from "@saibdev/bifrost";
+import { CardanoWallet, CardanoWalletApi, getWallets } from "@saibdev/bifrost";
+import { useCallback, useEffect, useState } from "react";
+import { SelectWalletModal } from "~/components/select_wallet_modal";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,17 +13,54 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [wallets, setWallets] = useState<CardanoWallet[]>(getWallets());
+  const [selectedWallet, setSelectedWallet] = useState<CardanoWallet>();
+  const [walletApi, setWalletApi] = useState<CardanoWalletApi>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setIsModalOpen((prev) => !prev);
+  }, []);
+
+  const handleSelectWallet = useCallback((wallet: CardanoWallet) => {
+    setSelectedWallet(wallet);
+    setIsModalOpen(false);
+  }, []);
+
+  useEffect(() => {
+    const handleSetWalletApi = async () => {
+      if (selectedWallet) {
+        const api = await selectedWallet.enable();
+        setWalletApi(api);
+      }
+    }
+    handleSetWalletApi();
+  }, [selectedWallet]);
+
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      {isModalOpen && <SelectWalletModal wallets={wallets} handleSelectWallet={handleSelectWallet} />}
       <div className="flex gap-4 flex-col w-[800px] h-[400px] bg-[#15191e] drop-shadow-xl rounded-lg p-5">
-
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
           <div className="w-[200px]">
             <img src="/logo.png" alt="Fortuna Logo" className="w-full h-full" />
           </div>
+          {selectedWallet ? (
+            <div className="flex items-center justify-center w-[50px] p-2 bg-[#15191e] rounded-md cursor-pointer 
+            hover:bg-[#4E5BE5] active:bg-[#3F4CCB] transition select-none" onClick={handleOpenModal}>
+              <div className="w-[200px]">
+                <img src={selectedWallet.icon} alt="Fortuna Logo" className="w-full h-full" />
+              </div>
+            </div>
+          ) :
+            <div className="flex items-center justify-center w-[200px] p-2 bg-[#5A66F6] rounded-md cursor-pointer 
+          hover:bg-[#4E5BE5] active:bg-[#3F4CCB] transition select-none" onClick={handleOpenModal}>
+              <span className="text-black font-medium">SELECT WALLET</span>
+            </div>
+          }
         </div>
 
-        <div className="w-full">
+        <div className="w-full flex justify-center items-center">
           <h1 className="text-[22px]">Convert Your V1 $TUNA</h1>
         </div>
 
