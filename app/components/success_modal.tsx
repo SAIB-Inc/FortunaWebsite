d5c1a9346a4d75e4d824ae92a230d44e263450ca
Mercurial <clark_alesna@hotmail.com>
2024-12-10 23:54:10 +0800

    fix: enhance success modal layout and improve loading state handling

--- app/components/success_modal.tsx
+++ app/components/success_modal.tsx
@@ -14,43 +14,31 @@ const SuccesModal = ({ onClose, isLoading, txId }: SuccesModalProps) => {
             <div
                 className="flex flex-col items-center w-[500px] bg-[#15191e] rounded-lg p-8 text-white"
             >
+                <div className="flex flex-col items-center p-4">
+                    <h2 className="text-white font-bold text-2xl mb-3">{isLoading ? "Hang Tight" : "Success"}</h2>
+                    <p className="text-[#D1D5DB] text-md mb-1 text-center p-4">
+                        {isLoading
+                            ? "We're waiting for confirmation on your transaction. Hang tight..."
+                            : "Your transaction has been successfully confirmed!"}
+                    </p>
+                    <a
+                        href={`https://cardanoscan.io/transaction/${txId}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[#00cdb8] hover:underline text-md"
+                    >
+                        View your transaction details here
+                    </a>
+                </div>
+
                 {isLoading ? (
-                    <div className="flex items-center justify-center w-16 h-16 rounded-full /10 mb-6">
+                    <div className="flex items-center justify-center w-16 h-16 rounded-full /10">
                         <div className="animate-spin rounded-full h-8 w-8 border-4 border-t-transparent border-[#00cdb8]"></div>
                     </div>
                 ) : (
-                    <div className="flex items-center justify-center w-16 h-16 rounded-full bg-[#00cdb8]/10 mb-6">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-8 w-8 text-[#00cdb8]"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={2.5}
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M5 13l4 4L19 7"
-                            />
-                        </svg>
-                    </div>
+                    <></>
                 )}
 
-                <h2 className="text-white font-bold text-2xl mb-3">{isLoading ? "Hang Tight" : "Success!"}</h2>
-                <p className="text-[#D1D5DB] text-md mb-1">
-                    {isLoading
-                        ? "We're waiting for confirmation on your transaction. Hang tight..."
-                        : "Your transaction has been successfully confirmed!"}
-                </p>
-                <a
-                    href={`https://cardanoscan.io/transaction/${txId}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-[#00cdb8] hover:underline text-md"
-                >
-                    View your transaction details here
-                </a>
                 {!isLoading ?
                     <div className="flex items-center justify-center pt-3 w-[150px]">
                         <button
